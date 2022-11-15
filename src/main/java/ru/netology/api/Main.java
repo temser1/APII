e3c9acb82dbfb45eef9026c19d365455cd2f3cf3
Serovtsev Artem <1doncarleon@gmail.com>
2022-11-15 19:06:08 +0300

    initial commit

---
+++ src/main/java/ru/netology/api/Main.java
@@ -0,0 +1,19 @@
+package ru.netology.api;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.patronymic = "Иванович";
+        post.surname = "Иванов";
+        post.passport = "4444 № 444444";
+        post.phone = "+7 (999)-99-99";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+
+    }
+}
---
+++ src/main/java/ru/netology/api/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.api;
+
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+
+    FormDate birthday = new FormDate();
+}
